e496449200b7b089dd0615fbe4559becf0bdcd9b
AjayBadrinath <92035508+AjayBadrinath@users.noreply.github.com>
2022-01-30 13:53:36 +0530

    atoi.c
    Convert charecter bytestream to int datatype

--- test.c
+++ test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <ctype.h>
-//int atoi(char a[]);
+int atoi(char a[]);
 int main(){
 int sum;
 int ab;
 scanf("%d",&ab);
 printf("%d\n",ab);
-/*
+
 for(int i=1;i<argc;i++){sum+=atoi(argv[i]);}
 printf("%d\n",sum);
 }
@@ -17,16 +17,6 @@ si=(*s=='-')?-1:1;
 if(*s=='+' ||*s=='-'){s++;}
 for (;isdigit(*s);s++){l=l*10+(*s-48);}
 return si*l;
-*/
-}
-/*
-char itoa(int a){
-char num[];
-int i=0;
-if(a<0){
-s[i]='-';
-i++;
-}
 
 }
-*/
+
